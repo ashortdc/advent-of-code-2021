ef53ad30ac9c5dff5c6a9ec49ce4604540386663
Adam Short <adam.short@dexcom.com>
2021-12-03 22:06:44 -0500

    Refactor day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -10,15 +10,18 @@ fn main() {
 
     let data = read_lines(&args[1]).unwrap();
 
-    println!(
-        "(Part 1) Measurements larger than the previous measurements: {}",
-        number_of_increases(&data, 1)
-    );
-
-    println!(
-        "(Part 2) Measurements larger than the previous measurements: {}",
-        number_of_increases(&data, 3)
-    );
+    for i in [1, 3] {
+        let mut count = 0;
+        let _ = &data[0..data.len() - i + 1]
+            .iter()
+            .zip(&data[i..data.len()])
+            .for_each(|(x, y)| {
+                if x < y {
+                    count += 1
+                }
+            });
+        println!("(Window = {}) Count: {}", i, count);
+    }
 }
 
 fn read_lines<P>(filename: P) -> Result<Vec<i32>, Error>
@@ -29,12 +32,4 @@ where
     br.lines()
         .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
         .collect()
-}
-
-fn number_of_increases(array: &Vec<i32>, window: usize) -> i32 {
-    let mut ret_value = 0;
-    for i in window..array.len() {
-        ret_value += if array[i] > array[i - window] {1} else {0};
-    }
-    ret_value
 }
